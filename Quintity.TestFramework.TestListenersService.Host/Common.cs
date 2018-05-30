594e727b7f0353cc2e261e758f86b41d232e7fef
James Mothershead <jmothershead@quintity.com>
2018-05-30 13:26:53 -0700

    Add project files.

---
+++ Quintity.TestFramework.TestListenersService.Host/Common.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Quintity.TestFramework.ListenersService.Host
+{
+    public class Common
+    {
+        internal static System.ServiceModel.ServiceHost _host = null;
+
+        internal static void StartWebService()
+        {
+            try
+            {
+                startService();
+            }
+            catch (Exception e)
+            {
+                Program.LogEvent.Fatal("Unhandled exception.", e);
+            }
+            finally
+            { }
+        }
+
+        internal static void startService()
+        {
+            _host = new System.ServiceModel.ServiceHost(typeof(Quintity.TestFramework.TestListenersService.ListenerEvents),
+                new Uri("net.tcp://localhost:10101//Quintity.TestFramework.TestListenersService/"));
+
+            //_host.Faulted += new EventHandler(_host_Faulted);
+
+            _host.Open();
+        }
+    }
+}
---
+++ Quintity.TestFramework.TestListenersService.Host/WinService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.ServiceProcess;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quintity.TestFramework.ListenersService.Host
+{
+    public class WinService : ServiceBase
+    {
+        public WinService()
+        { }
+
+        protected override void OnStart(string[] args)
+        {
+            try
+            {
+                //Common.ProcessJournalEntries();
+            }
+            catch (Exception e)
+            {
+                Program.LogEvent.Fatal("Unhandled exception.", e);
+                Stop();
+            }
+        }
+
+        protected override void OnStop()
+        {
+            Program.LogEvent.Info("Quintity.TestFramework.ListenersService stopped.");
+        }
+    }
+}
